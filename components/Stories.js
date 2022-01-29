5e41b1159a854e2d85ba1c83caffc5dbf5063db7
meer-habib-dev <contact.freelancermir@gmail.com>
2022-01-29 22:23:17 +0600

    updated

--- components/Stories.js
+++ components/Stories.js
@@ -1,15 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Story from "./Story";
 // import { Faker } from "react-fakers";
 const { faker } = require("@faker-js/faker");
 const Stories = () => {
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
-    const suggestions = [...Array(20)].map((_, i) => ({
+    const suggestion = [...Array(20)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }));
-    console.log(suggestions);
+    setSuggestions(suggestion);
   }, []);
-  return <div></div>;
+  console.log(suggestions);
+  return (
+    <div className="flex space-x-2 p-6 bg-white border-gray-300 border rounded-sm overflow-x-scroll mt-8">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Stories;
